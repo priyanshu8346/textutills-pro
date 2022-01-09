8b7e70b5e023e26d4fd091d60c741fd259c65e91
Priyanshu <2020eeb079.priyanshu@students.iiests.ac.in>
2022-01-09 13:07:22 +0530

    Updates

---
+++ precache-manifest.90bcf9dfe2584e36c6704fb1871d0dc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "763a5129555b9ceca243",
+    "url": "/Textutills-pro/static/css/main.df1d15d0.chunk.css"
+  },
+  {
+    "revision": "763a5129555b9ceca243",
+    "url": "/Textutills-pro/static/js/main.763a5129.chunk.js"
+  },
+  {
+    "revision": "dbf16a7a1f5d12f53526",
+    "url": "/Textutills-pro/static/js/1.dbf16a7a.chunk.js"
+  },
+  {
+    "revision": "3a2e0e0b763789a1abbb",
+    "url": "/Textutills-pro/static/js/2.3a2e0e0b.chunk.js"
+  },
+  {
+    "revision": "973fe7cd5c423c03b151",
+    "url": "/Textutills-pro/static/js/runtime~main.973fe7cd.js"
+  },
+  {
+    "revision": "bfad18a38bd2b117ed899756c588a810",
+    "url": "/Textutills-pro/index.html"
+  }
+];
